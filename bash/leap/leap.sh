918d2e6eb7fb34ebe2c783a6adeb1206eed92aa0
wanaga3166 <benoit-aliaga@orange.fr>
2019-11-08 17:40:09 +0100

    Script correct, mais exercism non

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -1,30 +1,11 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
-
 main() {
-
+    if [ $(($1 % 4)) -eq 0 ] && [ $(($1 % 100)) -ne 0 ] || [ $(($1 % 400)) -eq 0 ]
+        then echo "Leap"
+    else 
+        echo "Not Leap"
+    fi
 }
 
 main "$@"
