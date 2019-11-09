1ee3e8edfd581d55ffae684d85ced0c57111307a
wanaga3166 <benoit-aliaga@orange.fr>
2019-11-09 15:44:46 +0100

    Version finale

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -2,8 +2,17 @@
 # and should return the difference-of-squares as described
 # in the README.md
 difference_of_squares <- function(natural_number) {
-  # The square of the sum of the first ten natural numbers is
-   natural_number+h
-  # The sum of the squares of the first ten natural numbers is
+  ma_liste <- seq(1, natural_number)
   
+  # The square of the sum of the first ten natural numbers is
+  square_sum <- sum(ma_liste)^2
+  
+  # The sum of the squares of the first ten natural numbers is
+  sum_square <- sum(ma_liste^2)
+  
+  # Calculating the difference between the sum of the squares and the square of the sum.
+  difference <- square_sum - sum_square
+  return(difference)
 }
+
+#difference_of_squares(10)
